f9dfafb2205ccb48f4b67b89907b06bc5b53465d
slam-cheg <levashushkov@yandex.ru>
2023-09-05 08:50:36 +0300

    video js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const outerAccords = document.querySelectorAll(".accord-outer");
 const programAccord = document.querySelector(".program__accord-wrapper");
 const programAccordItems = programAccord.querySelectorAll(".accord-inner__element");
+const iframeLayout = `<iframe class="videos__iframe" width="100%" height="100%" frameborder="0" allowfullscreen="" src="" allow="autoplay"></iframe>`;
+const iframeWrappers = document.querySelectorAll(".videos__iframe-wrapper");
 
 outerAccords.forEach((accord) => {
 	const outerAccordElement = accord.querySelector(".accord-outer__element");
@@ -24,6 +26,20 @@ programAccordItems.forEach((element) => {
 	const programAccordHeader = element.querySelector(".accord-header");
 	programAccordHeader.addEventListener("click", () => {
 		handlerAccordToggle(element);
+	});
+});
+
+iframeWrappers.forEach((wrapper) => {
+	const playBtn = wrapper.querySelector(".videos__play");
+	const preview = wrapper.querySelector(".videos__preview");
+	const currentVideoId = wrapper.dataset.video;
+
+	wrapper.addEventListener("click", () => {
+		preview.insertAdjacentHTML("afterend", iframeLayout);
+		const iframe = wrapper.querySelector(".videos__iframe");
+		iframe.src = `https://www.youtube.com/embed/${currentVideoId}?&amp;autoplay=1&amp;rel=0`;
+		playBtn.classList.add("videos__play_hidden");
+		preview.classList.add("videos__preview_hidden");
 	});
 });
 
