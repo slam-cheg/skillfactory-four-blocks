56feafe575752b1fef2eeb55a22deccfce8962e2
slam-cheg <levashushkov@yandex.ru>
2023-09-08 09:50:22 +0300

    learning adaptive

--- scripts/index.js
+++ scripts/index.js
@@ -15,148 +15,195 @@ const reviewPopupCloseButton = reviewPopup.querySelector(".popup-review__close-i
 reviewPopupCloseButton.addEventListener("click", closePopup);
 
 outerAccords.forEach((accord) => {
-  const outerAccordElement = accord.querySelector(".accord-outer__element");
-  const outerAccordHeader = outerAccordElement.querySelector(".accord-header_outer");
-  const innerAccords = outerAccordElement.querySelectorAll(".accord-inner");
+	const outerAccordElement = accord.querySelector(".accord-outer__element");
+	const outerAccordHeader = outerAccordElement.querySelector(".accord-header_outer");
+	const innerAccords = outerAccordElement.querySelectorAll(".accord-inner");
 
-  outerAccordHeader.addEventListener("click", () => {
-    handlerAccordToggle(outerAccordElement);
-  });
-  innerAccords.forEach((innerAccord) => {
-    const innerAccordElements = innerAccord.querySelectorAll(".accord-inner__element");
-    innerAccordElements.forEach((element) => {
-      element.addEventListener("click", () => {
-        handlerAccordToggle(element);
-      });
-    });
-  });
+	outerAccordHeader.addEventListener("click", () => {
+		handlerAccordToggle(outerAccordElement);
+	});
+	innerAccords.forEach((innerAccord) => {
+		const innerAccordElements = innerAccord.querySelectorAll(".accord-inner__element");
+		innerAccordElements.forEach((element) => {
+			element.addEventListener("click", () => {
+				handlerAccordToggle(element);
+			});
+		});
+	});
 });
 
 programAccordItems.forEach((element) => {
-  const programAccordHeader = element.querySelector(".accord-header");
-  programAccordHeader.addEventListener("click", () => {
-    handlerAccordToggle(element);
-  });
+	const programAccordHeader = element.querySelector(".accord-header");
+	programAccordHeader.addEventListener("click", () => {
+		handlerAccordToggle(element);
+	});
 });
 
 iframeWrappers.forEach((wrapper) => {
-  const playBtn = wrapper.querySelector(".videos__play");
-  const preview = wrapper.querySelector(".videos__preview");
-  const currentVideoId = wrapper.dataset.video;
+	const playBtn = wrapper.querySelector(".videos__play");
+	const preview = wrapper.querySelector(".videos__preview");
+	const currentVideoId = wrapper.dataset.video;
 
-  wrapper.addEventListener("click", () => {
-    preview.insertAdjacentHTML("afterend", iframeLayout);
-    const iframe = wrapper.querySelector(".videos__iframe");
-    iframe.src = `https://www.youtube.com/embed/${currentVideoId}?&amp;autoplay=1&amp;rel=0`;
-    playBtn.classList.add("videos__play_hidden");
-    preview.classList.add("videos__preview_hidden");
-  });
+	wrapper.addEventListener("click", () => {
+		preview.insertAdjacentHTML("afterend", iframeLayout);
+		const iframe = wrapper.querySelector(".videos__iframe");
+		iframe.src = `https://www.youtube.com/embed/${currentVideoId}?&amp;autoplay=1&amp;rel=0`;
+		playBtn.classList.add("videos__play_hidden");
+		preview.classList.add("videos__preview_hidden");
+	});
 });
 
 reviews.forEach((slide) => {
-  const nameText = slide.querySelector(".videos__name").textContent;
-  const descriptionText = slide.querySelector(".videos__description").textContent;
-  const reviewButton = slide.querySelector(".videos__popup-button");
-  const subtitleText = reviewButton.dataset.course;
+	const nameText = slide.querySelector(".videos__name").textContent;
+	const descriptionText = slide.querySelector(".videos__description").textContent;
+	const reviewButton = slide.querySelector(".videos__popup-button");
+	const subtitleText = reviewButton.dataset.course;
 
-  reviewButton.addEventListener("click", () => {
-    openPopup(nameText, subtitleText, descriptionText);
-  });
+	reviewButton.addEventListener("click", () => {
+		openPopup(nameText, subtitleText, descriptionText);
+	});
 });
 
 function handlerAccordToggle(accord) {
-  accord.classList.toggle("accord-opened");
-  accord.classList.toggle("accord-closed");
+	accord.classList.toggle("accord-opened");
+	accord.classList.toggle("accord-closed");
 }
 
 const videosSwiper = new Swiper(".videos__swiper", {
-  loop: false,
-  navigation: {
-    nextEl: "#videos-prev-btn",
-    prevEl: "#videos-next-btn",
-  },
-  spaceBetween: 44,
-  slideToClickedSlide: false,
-  mousewheel: {
-    invert: false,
-    forceToAxis: true,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1.2,
-    },
-    480: {
-      slidesPerView: 1.8,
-      freeMode: {
-        enabled: true,
-        sticky: false,
-      },
-    },
-    640: {
-      slidesPerView: 2.4,
-    },
-    960: {
-      slidesPerView: 4,
-      freeMode: {
-        enabled: false,
-        sticky: true,
-      },
-    },
-  },
+	loop: false,
+	navigation: {
+		nextEl: "#videos-prev-btn",
+		prevEl: "#videos-next-btn",
+	},
+	spaceBetween: 44,
+	slideToClickedSlide: false,
+	mousewheel: {
+		invert: false,
+		forceToAxis: true,
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1.2,
+		},
+		480: {
+			slidesPerView: 1.8,
+			freeMode: {
+				enabled: true,
+				sticky: false,
+			},
+		},
+		640: {
+			slidesPerView: 2.4,
+		},
+		960: {
+			slidesPerView: 4,
+			freeMode: {
+				enabled: false,
+				sticky: true,
+			},
+		},
+	},
 });
 
 function openPopup(name, subtitle, description) {
-  reviewPopupName.textContent = name;
-  reviewPopupSubtitle.textContent = subtitle;
-  reviewPopupDescription.textContent = description;
-  reviewPopup.classList.add("popup-review_opened");
+	reviewPopupName.textContent = name;
+	reviewPopupSubtitle.textContent = subtitle;
+	reviewPopupDescription.textContent = description;
+	reviewPopup.classList.add("popup-review_opened");
 
-  reviewPopup.addEventListener("click", handleOverlayClick);
-  window.addEventListener("keydown", closeByEscape);
+	reviewPopup.addEventListener("click", handleOverlayClick);
+	window.addEventListener("keydown", closeByEscape);
 }
 
 function closePopup() {
-  reviewPopup.classList.remove("popup-review_opened");
-  setTimeout(clearPopup, 300);
-  window.removeEventListener("keydown", closeByEscape);
+	reviewPopup.classList.remove("popup-review_opened");
+	setTimeout(clearPopup, 300);
+	window.removeEventListener("keydown", closeByEscape);
 }
 
 function handleOverlayClick(event) {
-  if (event.target !== reviewPopupWrapper) {
-    closePopup();
-  }
+	if (event.target !== reviewPopupWrapper) {
+		closePopup();
+	}
 }
 
 function closeByEscape(event) {
-  if (event.key === "Escape") {
-    closePopup();
-  }
+	if (event.key === "Escape") {
+		closePopup();
+	}
 }
 
 function clearPopup() {
-  reviewPopupName.textContent = "####";
-  reviewPopupSubtitle.textContent = "####";
-  reviewPopupDescription.textContent = "####";
+	reviewPopupName.textContent = "####";
+	reviewPopupSubtitle.textContent = "####";
+	reviewPopupDescription.textContent = "####";
 }
 
-// const learningTabs = new Swiper(".learning__tabs", {
-//   loop: false,
-//   slideToClickedSlide: true,
-//   slidesPerView: 4,
+// document.querySelector(".tabs-button-next").addEventListener("click", () => {
+// 	learningVideos.slideNext();
+// });
+// document.querySelector(".tabs-button-prev").addEventListener("click", () => {
+// 	learningVideos.slidePrev();
 // });
 
-const learningVideos = new Swiper(".learning__videos", {
-  loop: false,
-  navigation: {
-    nextEl: ".learning-button-next",
-    prevEl: ".learning-button-prev",
-  },
-  slidesPerView: 1,
-  thumbs: {
-    swiper: {
-      el: '.learning__tabs',
-      slidesPerView: 4
-    }
-  }
-});
+if (window.innerWidth < 960) {
+	const thumbsSlider = new Swiper(".learning__tabs", {
+		loop: false,
+		slidesPerView: 4,
+		slideToClickedSlide: true,
+		navigation: {
+			nextEl: ".tabs-button-next",
+			prevEl: ".tabs-button-prev",
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			960: {
+				slidesPerView: 4,
+			},
+		},
+	});
 
+	const learningVideos = new Swiper(".learning__videos", {
+		loop: false,
+		navigation: {
+			nextEl: ".learning-button-next",
+			prevEl: ".learning-button-prev",
+		},
+		slidesPerView: 1,
+	});
+
+	learningVideos.controller.control = thumbsSlider;
+	thumbsSlider.controller.control = learningVideos;
+} else {
+	const thumbsSlider = new Swiper(".learning__tabs", {
+		loop: false,
+		slidesPerView: 4,
+		slideToClickedSlide: true,
+		navigation: {
+			nextEl: ".tabs-button-next",
+			prevEl: ".tabs-button-prev",
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			960: {
+				slidesPerView: 4,
+			},
+		},
+	});
+
+	const learningVideos = new Swiper(".learning__videos", {
+		loop: false,
+		navigation: {
+			nextEl: ".learning-button-next",
+			prevEl: ".learning-button-prev",
+		},
+		slidesPerView: 1,
+		thumbs: {
+			swiper: thumbsSlider,
+		},
+	});
+}
